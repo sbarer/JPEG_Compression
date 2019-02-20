5fba292c1cb2c0664c6ceb541ea2d910b34adc51
David <ddkpham@gmail.com>
2019-02-19 17:16:30 -0800

    adding initial UI setup for navbar and placeholder components

---
+++ src/containers/ImageManager/ImageManager.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import classes from './ImageManager.module.css';
+import Aux from '../../hoc/Aux'
+
+class ImageManager extends Component{
+    render(){
+        return(
+            <Aux>
+                <div>IMAGES</div>
+                <div>IMAGES</div>
+                <div>IMAGES</div>
+            </Aux>
+        );
+    }
+}
+
+export default ImageManager;
